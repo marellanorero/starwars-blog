9e68cf6089781428ffbd5ccbd16802e7e7a75611
marellanorero <marellanonorero@gmail.com>
2022-06-25 15:16:47 +0000

    terminado funcional

--- src/views/People.js
+++ src/views/People.js
@@ -14,28 +14,36 @@ const People = () => {
     return (
         <>
             <h1 className="mt-5 mb-5">PEOPLE</h1>
+            <div className="container">
+                            <div className="row">
             {
                 store.characters.map((character, index) => {
                     return (
-                                <div className="card col-md-4 mb-2" key={index} >
-                                    <div className="card-body">
-                                        <h5 className="card-title">{character.name}</h5>
-                                        <p className="card-text">
-                                            <li>Sex: {character.gender}</li>
-                                            <li>Hair Color: {character.hair_color}</li>
-                                            <li>Eye Color: {character.eye_color}</li>
-                                        </p>
-                                        <Link to={"/people/" + character.url.match(/\d+/)[0]}>
-                                            <button className="btn btn-primary" size="sm">
-                                                Learn more!
-                                            </button>
-                                        </Link>
-                                        <button className="btn btn-secondary" onClick={() => actions.addFavorites(character)}><FaHeart /></button>
+                       
+                                <div className="col-sm-12 col-md-2 col-lg-4">
+                                    <div className="card mb-2" key={index} >
+                                        <div className="card-body">
+                                            <h5 className="card-title">{character.name}</h5>
+                                            <p className="card-text">
+                                                <li>Sex: {character.gender}</li>
+                                                <li>Hair Color: {character.hair_color}</li>
+                                                <li>Eye Color: {character.eye_color}</li>
+                                            </p>
+                                            <Link to={"/people/" + character.url.match(/\d+/)[0]}>
+                                                <button className="btn btn-primary" size="sm">
+                                                    Learn more!
+                                                </button>
+                                            </Link>
+                                            <button className="btn btn-secondary" onClick={() => actions.addFavorites(character)}><FaHeart /></button>
+                                        </div>
                                     </div>
                                 </div>
+                            
                     )
                 })
             }
+            </div>
+                        </div>
         </>
     )
 }
